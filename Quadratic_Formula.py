57366bb94523500994c408e94a054533762c5f4f
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-10-21 18:07:47 -0400

    Add files via upload

---
+++ Quadratic_Formula.py
@@ -0,0 +1,19 @@
+import math
+name=input('Please enter your name')
+print()
+print('Welcome, ',name,', to the Quadratic Formula Calculator',sep=(''))
+input('Press ENTER to continue')
+print()
+a=float(input('Please enter the value of a:'))
+b=float(input('Please enter the value of b:'))
+c=float(input('Please enter the value of c:'))
+dd = (b**2) - (4*a*c)
+d = math.sqrt(dd)
+solo=(-b-(d))/(2*a)
+solt=(-b+(d))/(2*a)
+print()
+print('Calculating all possibilities...')
+print()
+print('''The possible solutions are either''',solo,'''
+            or
+       ''',solt)
